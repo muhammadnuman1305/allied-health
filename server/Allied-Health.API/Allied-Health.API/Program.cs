ce26b912e8349cb255a55e0449a0350c6797e461
muhammadnuman1305 <numanzahid91@gmail.com>
2025-09-15 14:59:44 +0500

    Added auto migration in Program.cs

--- server/Allied-Health.API/Allied-Health.API/Program.cs
+++ server/Allied-Health.API/Allied-Health.API/Program.cs
@@ -97,6 +97,11 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AlliedHealthDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
